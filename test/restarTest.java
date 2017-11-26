89076a19efff076ae8456bb357d91c225d84a9c5
Leans <leans177@gmail.com>
2017-11-26 16:18:24 -0300

    agregado test: restarNegativos

---
+++ test/restarTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class restarTest {
+	
+	Calculadora unaCalculadora = new Calculadora();
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void restarPositivos() {
+		int x = 5;
+		int y = 3;
+		String resta = unaCalculadora.restar(x, y);
+		assertTrue(resta.equals("2"));
+	}
+	
+	@Test
+	public void restarNegativos() {
+		int x = -5;
+		int y = -3;
+		String resta = unaCalculadora.restar(x, y);
+		assertTrue(resta.equals("-2"));
+	}
+
+}
